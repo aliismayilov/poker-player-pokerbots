96d3f58b52674b45d6f4d4a7e508cd2b48b023a1
Ali Ismayilov <ali@ismayilov.email>
2022-08-24 11:17:02 +0200

    call current in action player

--- player.rb
+++ player.rb
@@ -4,7 +4,7 @@ class Player
   VERSION = "call big blind"
 
   def bet_request(game_state)
-    game_state[:small_blind] * 2
+    game_state[:current_buy_in] - game_state[:players][game_state[:in_action]][:bet]
   end
 
   def showdown(game_state)
